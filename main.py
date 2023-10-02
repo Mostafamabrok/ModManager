e979d648fb9534aba001e8ed2de5f2985bbed9b6
Mostafa Mabrok <75213491+Mostafamabrok@users.noreply.github.com>
2023-10-02 17:42:25 +0300

    gave functionality to `mod_folder_config()`

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+import pickle
+
 def Introduction():
-    print("ModManager v0.0.1, Developed By MSTF Studios")
+    print("ModManager v0.0.1, Developed By MSTF Studios\n")
+    print ("Chose an action from the prompts by typing a number and pressing enter.")
     print("1-Create a new modset")
     print("2-Use a saved modset")
     print("3-View saved modsets")
-    print("3-Delete a saved modset")
+    print("4-Delete a saved modset")
+    print("5-Change or set mods folder")
+
+    desired_function = input (": ")
+
+    if desired_function == "1": save_modset()
+    if desired_function == "2": use_modset()
+    if desired_function == "3": view_modsets()
+    if desired_function == "4": delete_modset()
+    if desired_function == "5": mod_folder_config()
 
 
 def save_modset():
@@ -13,7 +25,20 @@ def use_modset():
     pass
 
 def view_modsets():
+    with open("modfolder.pickle", "rb") as f: print(pickle.load(f))
+    #pass
+
+def delete_modset():
     pass
 
-def delete_set():
-    pass
+def mod_folder_config():
+    permission = input("Do you want to chnage or set a mod folder? (y/n): ")
+
+    if permission == "y":
+        modfolder = input("Input the full path to your mod folder: ")
+        with open("modfolder.pickle", "wb") as f: pickle.dump(modfolder, f) 
+
+        
+    
+
+Introduction()
