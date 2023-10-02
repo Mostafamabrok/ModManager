433289cd9f6aee0d3a0b4677a666a465407dccfd
Mostafa Mabrok <75213491+Mostafamabrok@users.noreply.github.com>
2023-10-02 15:11:54 +0300

    Started Intro function and vewing feature

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def Introduction():
     print("ModManager v0.0.1, Developed By MSTF Studios")
+    print("1-Create a new modset")
+    print("2-Use a saved modset")
+    print("3-View saved modsets")
+    print("3-Delete a saved modset")
+
 
 def save_modset():
     pass
 
 def use_modset():
+    pass
+
+def view_modsets():
     pass
 
 def delete_set():
