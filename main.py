a108a3a2da1669aa64a68ff1a06a39ceb5888025
Mostafa Mabrok <75213491+Mostafamabrok@users.noreply.github.com>
2023-10-06 13:21:20 +0300

    intiatlzed some functions

--- main.py
+++ main.py
@@ -2,5 +2,17 @@ import os
 import shutil
 import pickle
 
+def initialize():
+    pass
+
 def introduction():
+    pass
+
+def save_modset(modset_name, modsets_directory, mods_path):
+    pass
+
+def use_modset(modset_name, modsets_directory, mods_path):
+    pass
+
+def delete_modset(modset_name, modsets,_directory):
     pass
