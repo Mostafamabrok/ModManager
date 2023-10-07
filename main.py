0e93bba61f7b20d20018c30fa0b41cf1ca0c42db
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-10-07 09:26:00 +0300

    Gave funtionality to `start()`

--- main.py
+++ main.py
@@ -41,7 +41,29 @@ def initialize():
     mc_mods_folder = MM_data_dict['mc_mods_folder']
 
 def start():
-    pass
+
+    print("ModManager v0.0.1, Developed By MSTF Studios\n")
+    print("Chose an action from the prompts by typing a number and pressing enter.")
+    print("1-Create a new modset")
+    print("2-Use a saved modset")
+    print("3-View saved modsets")
+    print("4-Check the contents of a modset")
+    print("5-Delete a saved modset")
+    print("6-Configure application settings")
+    print("7-Exit ")
+
+    desired_function = input (": ")
+
+    #Checks what the user wants to do and then executes the respective function.
+    if desired_function == "1": save_modset()
+    if desired_function == "2": use_modset()
+    if desired_function == "3": view_modsets()
+    if desired_function == "4": check_modset()
+    if desired_function == "5": delete_modset()
+    if desired_function == "6": data_setup(False)
+    if desired_function == "7": exit()
+
+    start()
 
 def save_modset(modset_name, modsets_directory, mc_mods_folder):
     pass
@@ -52,5 +74,12 @@ def use_modset(modset_name, modsets_directory, mc_mods_folder):
 def delete_modset(modset_name, modsets,_directory):
     pass
 
+def view_modsets():
+    pass
+
+def check_modset():
+    pass
+
 data_check()
 initialize()
+start()
