cbe85613ee8ef44fac179e808e594e73a768aa86
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2023-10-09 05:45:28 +0300

    Made print string shorter in

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def data_setup(data_ready):
 
     if data_ready == False:
         print("Starting ModManger"+version+" Config")
-        modsets_directory=input("Where do you want to store ModManager Modsets? Make sure to enter a full path. (or enter 'n' to store it where this script is located, this is reccomended):")
+        modsets_directory=input("Where do you want to keep your Modsets? ENTER FULL PATH (enter 'n' to store it in the cwd, (reccomended)):")
         mc_mods_folder=input("What is the path to your mods folder? (eg. C:/Users/me/AppData/Roaming/.minecraft/mods) (Make sure to enter the entire path!)")
 
         if modsets_directory == "n": 
