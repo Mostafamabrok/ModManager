b322910615ce8b6b0351694531edef9be3bfb57e
MostafaMarbok <mostafa.m.mabrok@gmail.com>
2023-10-12 20:25:08 +0300

    Simplified data_setup() if statement

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def data_check():
 
 def data_setup(data_ready):
 
-    if data_ready == False:
+    if not data_ready:
         print("Starting ModManger"+version+" Config")
         modsets_directory = input("Where will your Modsets be kept? ENTER FULL PATH (enter 'n' to store it in the cwd, (reccomended)):")
         mc_mods_folder = input("What is the path to your mods folder? (eg. C:/Users/(USERNAME)/AppData/Roaming/.minecraft/mods) (Make sure to enter the entire path!)")
